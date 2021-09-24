01cb80c767d108c96b458e7bacdc3c0c23876c33
Jiaxin Wang <jiaxin13@ualberta.ca>
2021-09-24 11:24:54 -0600

    Comelete sever

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 #  coding: utf-8 
-import socketserver
+from multiprocessing import connection
+import socketserver, os, socket
+from urllib.parse import urlparse
+import mimetypes
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -26,13 +29,114 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+PATH_PRIFIX = 'www'
 
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
-        self.data = self.request.recv(1024).strip()
+        self.data = self.recv_data(self.request)
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        data = self.data.decode('utf-8')
+        data = data.split('\r\n')
+        http_method, url_proto, http_version = data[0].split()
+
+        if http_method != "GET":
+            response_version = http_version
+            response_status = '405'
+            response_status_text = 'Not Allowed'
+
+            # sending all this stuff
+            r = '%s %s %s\r\n' % (response_version, response_status, response_status_text)
+            self.request.sendall((bytearray(r,'utf-8')))
+            return
+
+        url = PATH_PRIFIX + url_proto
+        url_path = urlparse(url).path
+
+        if url_path[-1] == '/':
+            url_path += 'index.html'
+        else:
+            if os.path.isdir(url_path):
+                url_proto += '/'
+                url_path = 'Location: ' + url_proto + '\r\n'
+                response_version = http_version
+                response_status = '301'
+                response_status_text = 'Moved Permanently'
+
+                # sending all this stuff
+                a = '%s %s %s\r\n' % (response_version, response_status, response_status_text)
+                self.request.sendall(bytearray(a,'utf-8'))
+                self.request.sendall(bytearray(url_path,'utf-8'))
+                self.request.sendall(bytearray('Cache-Control: no-cache\r\n','utf-8'))
+                return
+        
+        try:
+            with open(url_path, 'r') as f:
+                
+                data = f.read()
+                #print(data)
+                response_version = http_version
+                response_status = '200'
+                response_status_text = 'OK'
+                content_type = 'Content-type: ' + mimetypes.guess_type(url_path)[0] + '; charset=utf-8'
+                content_length = 'Content-Length: ' + str(len(data))
+                connection = 'Connection: close'
+
+                # print(content_type)
+
+
+                # sending all this stuff
+                header = '%s %s %s  \r\n' % (response_version, response_status, response_status_text)
+
+                header += content_type + '\r\n' + content_length + '\r\n' + connection + '\r\n' + '\r\n'
+
+                message = header + data
+                self.request.sendall(bytearray(message, 'utf-8'))
+                print("send filedwaaaaaaaaaaaaaaaaaa")
+                #print(message)
+                
+                #self.request.sendall(data)
+        except Exception as e:
+            print(e)
+            response_version = http_version
+            response_status = '404'
+            response_status_text = 'Not Found' # this can be random
+
+            # sending all this stuff
+            d = '%s %s %s\r\n' % (response_version, response_status, response_status_text)
+            self.request.sendall((bytearray(d,'utf-8')))
+        
+
+        
+
+        print(http_method)
+        print(url_path)
+        print(http_version)
+
+        
+
+        # url = urlparse(self.data)
+
+        #self.request.sendall((bytearray(HTTPStatus.OK,'utf-8')))
+        # self.request.sendall(bytearray("dawdafawdadwdadwaff",'utf-8'))
+        
+    def finish(self):
+        print("closed")
+        self.request.close()
+    
+    def recv_data(self, s):
+        s.settimeout(1)
+        full_data=b''
+        try:
+            while True:
+
+                data = s.recv(1024)
+                if not data or data == b'':
+                    break
+                full_data += data.strip()
+        except socket.timeout:
+            return full_data
+        return full_data
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
